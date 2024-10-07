bc582f659d5a57497c330fd7879d21494adb4553
jun <somang567@naver.com>
2024-10-07 13:48:29 +0900

    first commit

---
+++ src/main/java/com/keduit/interiors/InteriorsApplication.java
@@ -0,0 +1,13 @@
+package com.keduit.interiors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteriorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InteriorsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/keduit/interiors/InteriorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keduit.interiors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InteriorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
