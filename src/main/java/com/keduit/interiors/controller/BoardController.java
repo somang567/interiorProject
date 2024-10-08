d3df02a8c37b419d96456b2c2a03637e743cd3b6
jm <redcubix@naver.com>
2024-10-08 11:41:35 +0900

    시큐리티 막음 + 내가 하던 작업 ㅋㅋ

--- src/main/java/com/keduit/interiors/controller/BoardController.java
+++ src/main/java/com/keduit/interiors/controller/BoardController.java
@@ -2,12 +2,21 @@ package com.keduit.interiors.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BoardController {
 
     @GetMapping("/board/write")
-    public String boardwriteForm() {
+    public String boardWriteForm() {
         return "boards/boardwrite";
+    }
+
+    @PostMapping("/board/writedo")
+    public String boardWritePro(String title, String content) {
+
+        System.out.println("제목 : " + title);
+        System.out.println("내용 : " + content);
+        return "";
     }
 }
---
+++ src/main/java/com/keduit/interiors/entity/Board.java
@@ -0,0 +1,7 @@
+package com.keduit.interiors.entity;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Board {
+}
