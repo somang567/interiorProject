eb08eeab100fe9723b10e789869ff8d15203767a
jina <alslzkeb2@naver.com>
2024-10-09 22:07:13 +0900

    main 이것저것..

---
+++ src/main/java/com/keduit/interiors/dto/MainItemDTO.java
@@ -0,0 +1,27 @@
+/*
+package com.keduit.interiors.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class MainItemDTO {
+  private String itemNm;
+  private String itemDetail;
+  private int price;
+  private int stockNumber;
+  private String itemSellstatus;
+
+  public MainItemDTO(String itemNm, String itemDetail, Integer price, Integer stockNumber, String itemSellstatus) {
+    this.itemNm = itemNm;
+    this.itemDetail = itemDetail;
+    this.price = price;
+    this.stockNumber = stockNumber;
+    this.itemSellstatus = itemSellstatus;
+  }
+
+}
+*/
