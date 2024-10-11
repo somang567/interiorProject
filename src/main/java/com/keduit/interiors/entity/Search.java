daefe6f845a8d53bf259372d3a1180b071a0fe42
jm <redcubix@naver.com>
2024-10-11 12:06:11 +0900

    DB id pass 변경

--- src/main/java/com/keduit/interiors/entity/Search.java
+++ src/main/java/com/keduit/interiors/entity/Search.java
@@ -1,32 +1,32 @@
-package com.keduit.interiors.entity;
-
-import lombok.Data;
-
-import javax.persistence.*;
-
-// entity <-- (repository, service) --> controller : 엔티티와 컨트롤러를 레포지토리&서비스가 연결해줌.
-// Entity : 테이블 매핑. 데이터베이스 테이블의 구조를 나타내며, 해당 데이터를 어떻게 저장할지를 결정함
-@Entity
-@Data
-public class Search {
-
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column(name = "company_id")      // 열 이름 매핑
-  private long company_id;          // 순번(1,2,3,4 ...)
-
-  @Column(name = "company_name", nullable = false)  // null 허용X
-  private String company_name;      // 상호명
-
-  @Column(name = "job_type")
-  private String job_type;          // 업종
-
-  @Column(name = "road_address", nullable = false)  // null 허용X
-  private String road_address;      // 도로명 주소
-
-  @Column(name = "zip_number")
-  private String zip_number;        // 우편번호
-
-  @Column(name = "contact", nullable = false)       // null 허용X
-  private String contact;           // 전화번호
-}
+//package com.keduit.interiors.entity;
+//
+//import lombok.Data;
+//
+//import javax.persistence.*;
+//
+//// entity <-- (repository, service) --> controller : 엔티티와 컨트롤러를 레포지토리&서비스가 연결해줌.
+//// Entity : 테이블 매핑. 데이터베이스 테이블의 구조를 나타내며, 해당 데이터를 어떻게 저장할지를 결정함
+//@Entity
+//@Data
+//public class Search {
+//
+//  @Id
+//  @GeneratedValue(strategy = GenerationType.IDENTITY)
+//  @Column(name = "company_id")      // 열 이름 매핑
+//  private long company_id;          // 순번(1,2,3,4 ...)
+//
+//  @Column(name = "company_name", nullable = false)  // null 허용X
+//  private String company_name;      // 상호명
+//
+//  @Column(name = "job_type")
+//  private String job_type;          // 업종
+//
+//  @Column(name = "road_address", nullable = false)  // null 허용X
+//  private String road_address;      // 도로명 주소
+//
+//  @Column(name = "zip_number")
+//  private String zip_number;        // 우편번호
+//
+//  @Column(name = "contact", nullable = false)       // null 허용X
+//  private String contact;           // 전화번호
+//}
--- src/main/java/com/keduit/interiors/service/SearchService.java
+++ src/main/java/com/keduit/interiors/service/SearchService.java
@@ -1,13 +1,13 @@
-package com.keduit.interiors.service;
-
-import com.keduit.interiors.repository.SearchRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-// Service : 비즈니스 로직을 구현, 레포지토리와 연결되어 데이터 처리, 컨트롤러와 레포지토리 간의 중재 역할
-@Service
-public class SearchService {
-
-  @Autowired
-  private SearchRepository searchRepository;    // 객체 생성, 레포지토리의 내용 주입 가능
-}
+//package com.keduit.interiors.service;
+//
+//import com.keduit.interiors.repository.SearchRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//// Service : 비즈니스 로직을 구현, 레포지토리와 연결되어 데이터 처리, 컨트롤러와 레포지토리 간의 중재 역할
+//@Service
+//public class SearchService {
+//
+//  @Autowired
+//  private SearchRepository searchRepository;    // 객체 생성, 레포지토리의 내용 주입 가능
+//}
