c5e74bf673b3d5f2b339c91697e61fc6bca21d90
jina <alslzkeb2@naver.com>
2024-10-12 21:51:27 +0900

    헤더에 로고-메인, 게시판-게시판리스트 태그 걸어둠

--- src/main/java/com/keduit/interiors/config/AuditConfig.java
+++ src/main/java/com/keduit/interiors/config/AuditConfig.java
@@ -12,7 +12,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class AuditConfig {
 
   @Bean
-  public AuditorAware<String> auditorProvider(){
+  public AuditorAware<String> auditorProvider() {
     // 등록자, 수정자를 처리해주는 AuditorAwareImpl를 빈으로 등록
     return new AuditorAwareImpl();
   }
