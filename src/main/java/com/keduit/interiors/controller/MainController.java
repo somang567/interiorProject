d7b76b6fa8acbc270d223787ae662244b71961bf
jina <alslzkeb2@naver.com>
2024-10-14 11:49:03 +0900

    main list 불러오기 성공

--- src/main/java/com/keduit/interiors/controller/MainController.java
+++ src/main/java/com/keduit/interiors/controller/MainController.java
@@ -4,12 +4,12 @@ import com.keduit.interiors.entity.Board;
 import com.keduit.interiors.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
@@ -19,10 +19,11 @@ public class MainController {
 
 
   @GetMapping("/")
-  public String main(@RequestParam(defaultValue = "0") int page, Model model) {
+  public String main(Model model,
+                     @PageableDefault(page = 0, size = 10, sort = "id",
+                         direction = Sort.Direction.DESC) Pageable pageable) {
 
     // 리스트 부분
-    Pageable pageable = PageRequest.of(page, 10);  // 페이지당 10개씩 출력
     Page<Board> list = boardService.boardList(pageable);
 
     model.addAttribute("list", list);
