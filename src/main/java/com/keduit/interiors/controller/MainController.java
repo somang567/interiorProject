918e900d71297559c2839604a8157de60e512237
jina <alslzkeb2@naver.com>
2024-10-14 11:06:56 +0900

    main list 타임리프 및 컨트롤러

--- src/main/java/com/keduit/interiors/controller/MainController.java
+++ src/main/java/com/keduit/interiors/controller/MainController.java
@@ -1,24 +1,42 @@
 package com.keduit.interiors.controller;
 
+import com.keduit.interiors.entity.Board;
+import com.keduit.interiors.service.BoardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
 public class MainController {
 
+  private final BoardService boardService;
+
+
+  @GetMapping("/")
+  public String main(@RequestParam(defaultValue = "0") int page, Model model) {
+
+    // 리스트 부분
+    Pageable pageable = PageRequest.of(page, 10);  // 페이지당 10개씩 출력
+    Page<Board> list = boardService.boardList(pageable);
+
+    model.addAttribute("list", list);
+    return "main";  // main.html로 이동
+  }
+
 /*  @GetMapping("/")
-  public String main(Optional<Integer> page, Model model) {
-    Pageable pageable = PageRequest.of(page.isPresent() ? page.get() : 0, 4);
-    *//*model.addAttribute("item", items);*//*
+  public String main() {
 
     return "main";
   }*/
 
-  @GetMapping("/")
-  public String main() {
-
-    return "main";
+  @GetMapping("/mypage")
+  public String mypage() {
+    return "member/mypage";
   }
 }
