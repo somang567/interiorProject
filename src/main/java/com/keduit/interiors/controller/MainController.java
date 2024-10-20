ce508d2beba4ddd336d4ce67e92e9e3faf6eda78
jina <alslzkeb2@naver.com>
2024-10-20 22:07:09 +0900

    로그인 회원가입 css

--- src/main/java/com/keduit/interiors/controller/MainController.java
+++ src/main/java/com/keduit/interiors/controller/MainController.java
@@ -2,13 +2,17 @@ package com.keduit.interiors.controller;
 
 import com.keduit.interiors.dto.BoardDTO;
 import com.keduit.interiors.entity.Megazine;
+import com.keduit.interiors.entity.Member;
 import com.keduit.interiors.service.BoardService;
 import com.keduit.interiors.service.MegazineService;
+import com.keduit.interiors.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +23,7 @@ public class MainController {
 
   private final BoardService boardService;
   private final MegazineService megazineService;
+  private final MemberService memberService;
 
 
   @GetMapping("/")
@@ -37,15 +42,18 @@ public class MainController {
     return "main";  // main.html로 이동
   }
 
-
-/*  @GetMapping("/")
-  public String main() {
-
-    return "main";
-  }*/
-
   @GetMapping("/mypage")
-  public String mypage() {
-    return "member/mypage";
+  public String mypage(Model model, @AuthenticationPrincipal UserDetails userDetails) {
+    String email = userDetails.getUsername(); // 로그인된 사용자의 이메일 가져오기
+    Member member = memberService.findByEmail(email); // 이메일로 회원 조회
+
+    if (member != null) {
+      model.addAttribute("name", member.getName());
+      model.addAttribute("email", member.getEmail());
+    }
+
+    return "mypage"; // mypage.html로 이동
   }
+
+
 }
